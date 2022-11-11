1c236c949036e5e1edc8e984b8c9b4735e888a17
Rishabh Kumar <rishabh2807s@gmail.com>
2022-11-12 01:25:36 +0530

    Logic - Added intial logic for ste countdown timer. Style- added styles for the countdown timer

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -5,4 +5,18 @@ let hrs = document.getElementById("hour");
 let mins = document.getElementById("minute");
 let secs = document.getElementById("sec");
 
-let timer = null;
+let interval = null;
+
+function timer() {}
+
+function stoptimer() {
+  clearInterval(interval);
+}
+
+start.addEventListener("click", timer);
+reset.addEventListener("click", () => {
+  hrs.value = 0;
+  mins.value = 0;
+  secs.value = 0;
+  stoptimer();
+});
