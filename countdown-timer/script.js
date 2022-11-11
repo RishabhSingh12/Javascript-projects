31debcb810f526e0bc07a830def46fc757b02148
Rishabh Kumar <rishabh2807s@gmail.com>
2022-11-12 01:30:17 +0530

    Logic - Added logic for edge cases for the countdown timer.

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -7,13 +7,21 @@ let secs = document.getElementById("sec");
 
 let interval = null;
 
-function timer() {}
+function timer() {
+  if (secs.value === 0 || interval) {
+    return;
+  }
+  secs.value--;
+}
 
 function stoptimer() {
   clearInterval(interval);
 }
 
-start.addEventListener("click", timer);
+start.addEventListener("click", () => {
+  interval = setInterval(timer, 1000);
+});
+
 reset.addEventListener("click", () => {
   hrs.value = 0;
   mins.value = 0;
