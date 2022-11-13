aa9270187ba4ef83a9554d2608595984fa707b69
Rishabh Kumar <rishabh2807s@gmail.com>
2022-11-14 00:00:30 +0530

    Logic - Modified the add note logic part , modified logic for displayin notes.

--- Notes app/script.js
+++ Notes app/script.js
@@ -36,4 +36,15 @@ function displaynotes() {
   } else {
     notesobj = JSON.parse(notes);
   }
+
+  let html = "";
+  notesobj.forEach((ele, idx) => {
+    html += `<div id="note">
+                    <p class="note-counter">Note 1</p>
+                    <h3 class="note-title">Hello World</h3>
+                    <p class="note-text">Some Note text</p>
+                    <button class="note-btn">Delete</button>
+                    <button class="note-btn edit-btn">Edit</button>
+                </div>`;
+  });
 }
