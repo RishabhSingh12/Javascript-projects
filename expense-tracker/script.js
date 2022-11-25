bb6ddb6090e2b0116a6b129ffa86cbf910206f0e
Rishabh Kumar <rishabh2807s@gmail.com>
2022-11-25 20:57:23 +0530

    Added inital logic for the different sections of the expense manager app

---
+++ expense-tracker/script.js
@@ -0,0 +1,11 @@
+const balanceEl = document.queryselector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelectyor(".tab3");
+
+const expenseEl = document.querySelector("#expense");
+const incomeEl = document.querySelector("#income");
+const allEl = document.querySelector("#all");
