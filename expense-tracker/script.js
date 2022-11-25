af738927cb7f8ff2466a2ee980c804e1bf41b6f2
Rishabh Kumar <rishabh2807s@gmail.com>
2022-11-25 21:07:57 +0530

    Logic - Added logic for the display of different tab lists on user selection

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -9,3 +9,25 @@ const allBtn = document.querySelectyor(".tab3");
 const expenseEl = document.querySelector("#expense");
 const incomeEl = document.querySelector("#income");
 const allEl = document.querySelector("#all");
+
+// toggling logic for lists
+
+function active(ele) {
+  ele.classList.add("active");
+}
+
+function show(ele) {
+  ele.classList.remove("hide");
+}
+
+function hide(eleArr) {
+  eleArr.forEach((ele) => {
+    ele.classList.add("hide");
+  });
+}
+
+function inActive(eleArr) {
+  eleArr.forEach((ele) => {
+    ele.classList.remove("active");
+  });
+}
