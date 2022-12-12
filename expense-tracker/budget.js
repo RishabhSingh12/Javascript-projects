8469deb787f998ba099f4e8aee2613629db622ec
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-12 21:06:49 +0530

    Added required logic for the expense calculation function

---
+++ expense-tracker/budget.js
@@ -0,0 +1,11 @@
+const balanceEl = document.queryselector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+
+const expenseEl = document.querySelector("#expense");
+const incomeEl = document.querySelector("#income");
+const allEl = document.querySelector("#all");
+
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#outcome .list");
+const allList = document.querySelector("#all .list");
