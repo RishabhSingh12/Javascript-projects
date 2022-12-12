eb8b45f294e07dde64b7433a73e7aecb617a7e90
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-12 23:44:34 +0530

    Added required assets, logic , structure and styling for the shopping-cart project.

---
+++ shopping-cart/script.js
@@ -0,0 +1,24 @@
+import { cartdata as data } from "./cartdata.js";
+
+const root = document.querySelector(".root1");
+
+let html = "";
+let res = data.map((ele) => {
+  html =
+    html +
+    `<div class="w-[75%] p-6 border-gray-300 border-2 rounded-xl md:w-[40%] flex-col justify-center items-center text-center shadow-xl bg-white space-y-4">
+        <div class="img-container flex justify-center">
+            <img src='${ele?.image}' class="md:h-[350px] h-[250px] "/>
+        </div>
+        <p class="md:w-[80%] mx-auto">${ele?.title}</p>
+        <p class="font-bold"><span>Price-</span> $ ${ele?.price}</p>
+        <p>Qty- ${ele.rating?.count}</p>
+        <button class="p-2 bg-blue-900 shadow-lg rounded text-white w-[150px]">Add to Cart</button>
+    </div>
+    <br/>
+    `;
+});
+
+// console.log(res);
+
+root.innerHTML = html;
