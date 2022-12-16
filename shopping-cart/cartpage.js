6ccbbda1766522fb4aa07a40c305e7ab8462c3d3
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-16 22:10:20 +0530

    Modified logic for cartPage additems function and cartpage structure along with importing of some required modules

--- shopping-cart/cartpage.js
+++ shopping-cart/cartpage.js
@@ -1,3 +1,13 @@
+// import { cartdata as data } from "./cartdata";
 let cartitems = [];
 
-export const addItems = () => {};
+export const addItems = (html, id) => {
+  let demo = html.split("<br/>")[id - 1];
+  //   console.log(demo)
+  console.log(demo.includes(id));
+  //   if (demo.includes(id)) {
+  //     let foundItem = cartitems.find((ele) => {
+  //       ele.id === id;
+  //     });
+  //   }
+};
